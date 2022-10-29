486352b347cf2857d59cc98015c8d016c366eecd
shakhzodabdullaev <shakhzod.6673@gmail.com>
2022-10-29 13:18:50 +0500

    storage added

--- storage /storage.go
+++ storage /storage.go
@@ -1,4 +1,24 @@
 package storage
 
-type storageI interface {
+import "bootcamp/article/models"
+
+type StorageI interface {
+	Article() ArticleRepoI
+	Author() AuthorRepoI
+}
+
+type ArticleRepoI interface {
+	Create(entity models.ArticleCreateModel) (err error)
+	GetList(query models.Query) (resp []models.ArticleListItem, err error)
+	GetByID(ID int) (resp models.Article, err error)
+	Update(entity models.ArticleUpdateModel) (effectedRowsNum int, err error)
+	Delete(ID int) (effectedRowsNum int, err error)
+}
+
+type AuthorRepoI interface {
+	Create(entity models.Person) (err error)
+	GetList(query models.Query) (resp []models.Person, err error)
+	GetByID(ID int) (resp models.Person, err error)
+	Update(entity models.Person) (effectedRowsNum int, err error)
+	Delete(ID int) (effectedRowsNum int, err error)
 }
